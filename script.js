ac0610e9d368b9dd1ff54cccfdcfffaee1a727ff
kamuli3t <kamuli3t@gmail.com>
2025-01-30 07:51:27 -0800

    Part - 3 Done!

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-// const mainEl = document.getElementsByTagName("main");
-// mainEl.style.backgroundColor = "var(--main-bg)";
+const menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+//! Part - 1 Getting Started
+
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+mainEl.classList.add("flex-ctr");
+
+//! Part - 2 Creating a Menu Bar
+
+const topMenuEl = document.getElementById("top-menu");
+
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+topMenuEl.classList.add("flex-around");
+
+//! Part - 3 Adding Menu Buttons
+
+menuLinks.forEach((el) => {
+  let menuLinks = document.createElement("a");
+  menuLinks.setAttribute("href", el.href);
+  menuLinks.textContent = el.text;
+  topMenuEl.appendChild(menuLinks);
+});
+
+//! Part - 4 Adding Interactivity
+//* For Tomorrow
